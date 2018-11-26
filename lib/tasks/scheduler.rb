582cdb20e25eb8106f9a9c324567bdd24a88e5b7
Your Name <you@example.com>
2018-11-26 17:09:47 +1100

    heroku

--- lib/tasks/scheduler.rb
+++ lib/tasks/scheduler.rb
@@ -2,10 +2,10 @@ desc "This task is called by the Heroku scheduler add-on"
 
 task :update_feed => :environment do
   puts "Updating feed..."
-  NewsFeed.update
+  FetchItemsJob.new.perform
   puts "done."
 end
 
-task :send_reminders => :environment do
-  User.send_reminders
-end
+# task :send_reminders => :environment do
+#   User.send_reminders
+# end
