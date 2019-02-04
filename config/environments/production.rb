8ce6721e7d4ab5412c9f3c730dc6a9c67e00c7d4
Your Name <you@example.com>
2019-02-04 15:47:10 +1100

    resubscribe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'bitsandatoms.co' }
+  config.action_mailer.default_url_options = { host: 'ainews.bitsandatoms.co' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: 'key-9f02aa362fdf1017dc9846e065465b44',
